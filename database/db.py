6eec0223fc8db8790a27806ffc1c6da3c9188e84
emiranivala <taitufam@gmail.com>
2025-01-13 23:25:56 +0530

    Update db.py

--- database/db.py
+++ database/db.py
@@ -1,15 +1,28 @@
 import motor.motor_asyncio
-from config import DB_URI, DB_NAME
 
 class Database:
-    def __init__(self, uri, db_name):
+    def __init__(self, uri, database_name):
         self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
-        self.db = self._client[db_name]
+        self.db = self._client[database_name]
+        self.col = self.db.users  # The collection for users
 
-    # Define your database methods here
+    async def is_user_exist(self, id):
+        user = await self.col.find_one({'id': int(id)})
+        return bool(user)
+
+    async def add_user(self, id, name):
+        user = {"id": id, "name": name, "session": None}
+        await self.col.insert_one(user)
+
+    async def set_session(self, id, session):
+        await self.col.update_one({'id': int(id)}, {'$set': {'session': session}})
+
+    async def get_session(self, id):
+        user = await self.col.find_one({'id': int(id)})
+        return user['session'] if user else None
 
 # Initialize the database
-db = Database(DB_URI, DB_NAME)
+DB_URI = "your_mongodb_uri_here"
+DB_NAME = "your_database_name_here"
 
-async def initialize_db():
-    print(f"Connected to database: {db.db.name}")
+db = Database(DB_URI, DB_NAME)
