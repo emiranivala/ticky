2d80a5aa2c39a592d608de35e0a88674dbfae915
emiranivala <taitufam@gmail.com>
2025-01-13 20:15:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,12 @@
 import os
-from dotenv import load_dotenv
 
-# Load environment variables from .env file
-load_dotenv()
-
-
-# Debug print
-print(f"API_ID: {os.getenv('API_ID')}")  # This will show what is being loaded
-
+# Bot token from @BotFather
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.getenv("API_ID", "0"))  # Default to 0 if not found
-API_HASH = os.environ.get("API_HASH", "")
-DB_URI = os.environ.get("DB_URI", "")
+
+# Telegram API credentials from my.telegram.org
+API_ID = int(os.environ.get("API_ID", "24210243"))
+API_HASH = os.environ.get("API_HASH", "509031fb3790b968e489f71d591ebce5")
+
+# MongoDB URI
+DB_URI = os.environ.get("DB_URI", "")  # MongoDB connection string
+DB_NAME = "bot_database"  # Default database name
