4110cf03277682bf77ab190481afc7db815c0625
emiranivala <taitufam@gmail.com>
2025-01-13 19:34:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,11 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
 
-# Bot token from @Botfather
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 
-# Your API ID from my.telegram.org
-API_ID = int(os.getenv("API_ID", "20259558"))
+# Debug print
+print(f"API_ID: {os.getenv('API_ID')}")  # This will show what is being loaded
 
-# Your API Hash from my.telegram.org
-API_HASH = os.getenv("API_HASH", "ce95ed6fecb559fecbb6fb7ebed176e4")
-
-# Database URI
-DB_URI = os.getenv("DB_URI", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+API_ID = int(os.getenv("API_ID", "0"))  # Default to 0 if not found
+API_HASH = os.environ.get("API_HASH", "")
+DB_URI = os.environ.get("DB_URI", "")
